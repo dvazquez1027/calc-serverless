eaf0fd6d25fe4f2c7172fc5473f0a61d61cb6c9c
David Vazquez <david.vazquez@vetsez.com>
2021-07-30 22:48:34 -0500

    Allow overriding default command line

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-sam local start-api --docker-network calc-serverless_calc-serverless --profile local --region us-east-1 --env-vars env.json --warm-containers=EAGER
+cd "$( dirname ${BASH_SOURCE[0]})"
+
+#sam build
+
+if [ $# -lt 1 ]; then
+    echo "Running sam local start-api with default parameters."
+    sam local start-api --docker-network calc-serverless_calc-serverless --profile local --region us-east-1 --env-vars env.json --warm-containers=EAGER
+else
+    echo "Running sam local start-api with provided parameters."
+    sam local start-api "$@"
+fi
